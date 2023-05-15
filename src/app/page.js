faee766db4d73cb7b8149371804224368edae31e
Sajidwebsite <msjdkhan9@gmail.com>
2023-05-16 00:14:35 +0600

    fetch data in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,27 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Result } from "postcss";
 
+const API_KEY = process.env.API_KEY
+export default async function  Home({searchParams}) {
+  const genre = searchParams.genre|| "fetchTrending";
+  const res = await fetch(
+    `http://api.themoviedb.org/3/${
+      genre==="fetchTopRated" ? "movie/top_rated" : "trending/all/week"
 
-const inter = Inter({ subsets: ['latin'] })
+    }?api_key=${API_KEY}&language=en-US&page=1`, 
+    {next:{revalidate:10000}}
+   );
 
-export default function Home() {
+   if (!res.ok){
+    throw new Error("Failed to fetch data");
+   }
+
+   const data = await res.json();
+
+   const results = data.results;
+
+  
+
   return (
-   <div></div>
+   <div><Result results={results}/></div>
   )
 }
---
+++ src/components/Results/Results.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Results({results}) {
+  return (
+    <div>
+        {results.map((results) =>(
+            <div key={results.id}>
+                {results.original_title}
+            </div>
+
+        ))}
+    </div>
+  )
+}
